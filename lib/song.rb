ad8612b3032eaeefffd90f05f4c24dde79ca2fff
Alex Zhang <alexgzhang92@gmail.com>
2018-06-02 20:42:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,4 +62,43 @@ class Song
     self.all << song
     song
   end
+  
+  describe "Song" do
+  describe ".new_from_filename" do
+    it "initializes a song based on the passed-in filename" do
+      song = Song.new_from_filename("Thundercat - For Love I Come - dance.mp3")
+
+      expect(song.name).to eq("For Love I Come")
+      expect(song.artist.name).to eq("Thundercat")
+      expect(song.genre.name).to eq("dance")
+    end
+
+    it "invokes the appropriate Findable methods so as to avoid duplicating objects" do
+      artist = Artist.create("Thundercat")
+      genre = Genre.create("dance")
+
+      expect(Artist).to receive(:find_or_create_by_name).and_return(artist)
+      expect(Genre).to receive(:find_or_create_by_name).and_return(genre)
+
+      song = Song.new_from_filename("Thundercat - For Love I Come - dance.mp3")
+
+      expect(song.artist).to be(artist)
+      expect(song.genre).to be(genre)
+    end
+  end
+
+  describe ".create_from_filename" do
+    it "initializes and saves a song based on the passed-in filename" do
+      song = Song.create_from_filename("Thundercat - For Love I Come - dance.mp3")
+
+      expect(Song.all.last.genre.name).to eq("dance")
+    end
+
+    it "invokes .new_from_filename instead of re-coding the same functionality" do
+      expect(Song).to receive(:new_from_filename).and_return(double(save: true))
+
+      Song.create_from_filename("Thundercat - For Love I Come - dance.mp3")
+    end
+  end
+end
 end
