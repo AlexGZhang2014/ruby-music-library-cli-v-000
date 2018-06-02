7dcf6856c9fd01907d1e06e91068b357c32c0790
Alex Zhang <alexgzhang92@gmail.com>
2018-06-02 21:56:33 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,7 +19,9 @@ class MusicLibraryController
     list_songs if input == "list songs"
     list_artists if input == "list artists"
     list_genres if input == "list genres"
-    list_songs_by_artist if input == "list "
+    list_songs_by_artist if input == "list artist"
+    list_songs_by_genre if input == "list genre"
+    play_song if input == "play song"
     
     call unless input == "exit"
   end
@@ -76,55 +78,8 @@ class MusicLibraryController
     end
   end
 
-  describe "'list songs'" do
-    it "triggers #list_songs" do
-      allow(music_library_controller).to receive(:gets).and_return("list songs", "exit")
 
-      expect(music_library_controller).to receive(:list_songs)
-
-      capture_puts { music_library_controller.call }
-    end
-  end
-
-  describe "'list artists'" do
-    it "triggers #list_artists" do
-      allow(music_library_controller).to receive(:gets).and_return("list artists", "exit")
-
-      expect(music_library_controller).to receive(:list_artists)
-
-      capture_puts { music_library_controller.call }
-    end
-  end
-
-  describe "'list genres'" do
-    it "triggers #list_genres" do
-      allow(music_library_controller).to receive(:gets).and_return("list genres", "exit")
-
-      expect(music_library_controller).to receive(:list_genres)
-
-      capture_puts { music_library_controller.call }
-    end
-  end
-
-  describe "'list artist'" do
-    it "triggers #list_songs_by_artist" do
-      allow(music_library_controller).to receive(:gets).and_return("list artist", "exit")
-
-      expect(music_library_controller).to receive(:list_songs_by_artist)
-
-      capture_puts { music_library_controller.call }
-    end
-  end
-
-  describe "'list genre'" do
-    it "triggers #list_songs_by_genre" do
-      allow(music_library_controller).to receive(:gets).and_return("list genre", "exit")
-
-      expect(music_library_controller).to receive(:list_songs_by_genre)
-
-      capture_puts { music_library_controller.call }
-    end
-  end
+  
 
   describe "'play song'" do
     it "triggers #play_song" do
